b89907da465a0297cb6de34d34fabdbe8cc0cca7
OleksandraF705 <fiskovaal@gmail.com>
2024-06-12 13:46:18 +0200

    Updates related to https://trello.com/c/PPqX2YiC/3-ticket3-landing-page-add-test-admin-user-logged-in-to-the-account

--- pages/landing_page.js
+++ pages/landing_page.js
@@ -21,14 +21,42 @@ constructor (page) {
     // Store all Page elements that will be used during the testing
     this.titleLogin = page.getByRole('heading', { name: 'Login' });
 
+    this.userNamePlaceholder = page.getByPlaceholder('Username');    // the placeholder of the “Username“ field
+    this.passwordPlaceholder = page.getByPlaceholder('Password');   // the placeholder of the “Password“ field
+    this.loginButton = page.getByRole('button', {name: 'Login'});    // the [Login] button
+    this.dashboardTopbarHeader = page.getByRole('heading', {name: 'Dashboard'})    // the “Dashboard“ topbar-header
+
 }
 
 // Add async functions with detailed steps that will be used in the test spec file
-async redirectToOrangehrmLiveLandingPage() {
-    await this.page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
-    await this.page.waitForTimeout(2000);
-    await expect(this.titleLogin).toBeVisible();
-}
+
+    // the User redirection to the OrangehrmLive "Landing" page
+    async redirectToOrangehrmLiveLandingPage() {
+        await this.page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+        await this.page.waitForTimeout(2000);
+        await expect(this.titleLogin).toBeVisible();
+    }
+
+    // Fill in the “Username“ input with valid Admin User username.
+    async fillInUsernameFildWithAdminUsername(adminUsername) {
+        await this.userNamePlaceholder.fill(adminUsername);
+    }
+
+    // Fill in the “Password“ input with valid Admin User password.
+    async fillInPasswordFildWithAdminPassword(adminPassword) {
+        await this.passwordPlaceholder.fill(adminPassword);
+    }
+
+    // Click the “Login“ button.
+    async clickLoginButton() {
+        await this.loginButton.click();
+    }
+
+    // Check that “Dashboard“ topbar-header is displayed.
+    async checkDashboardTopbarHeader() {
+        await expect(this.dashboardTopbarHeader).toBeVisible();
+    }
+
 
 }
 
--- tests/admin.spec.js
+++ tests/admin.spec.js
@@ -6,16 +6,37 @@ const { test, expect } = require ('@playwright/test');
 // Include LandingPage model
 const { LandingPage } = require ('../pages/landing_page');
 
+
 // Create test
 test('The User is redirected to the "Landing page"', async ({page}) => {
     
     // Create variable for LandingPage class
     const landingPage = new LandingPage(page);
 
-    // Add step "goto google Home Page"
+    // Redirection to the Landing Page.
     await landingPage.redirectToOrangehrmLiveLandingPage();
 
 })
-kbkhv
+
+test('The Admin User successfully logged in to the account', async ({ page }) => {
+    
+    // Redirection to the Landing Page.
+    const landingPage = new LandingPage(page);
+    await landingPage.redirectToOrangehrmLiveLandingPage();
+
+    // Fill in the “Username“ input with valid Admin User username.
+    await landingPage.fillInUsernameFildWithAdminUsername('Admin');
+
+    // Fill in the “Password“ input with valid Admin User password.
+    await landingPage.fillInPasswordFildWithAdminPassword('admin123')
+
+    // Click the “Login“ button.
+    await landingPage.clickLoginButton();
+    await page.waitForTimeout(2000);
+
+    // Check that “Dashboard“ topbar-header is displayed.
+    await landingPage.checkDashboardTopbarHeader();
+
+})
 
 
